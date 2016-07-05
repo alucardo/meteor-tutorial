ed93090d53d03da05ae9090ea2e8eefb13d9a53d
przemek4mm <przemek.4mm@gmail.com>
2016-07-05 11:47:16 +0200

    single subscription

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,8 @@
 Meteor.publish('recipies', function(){
   return Recipies.find({author: this.userId});
 })
+
+Meteor.publish('singleRecipe', function(id){
+  check(id, String);
+  return Recipies.find({_id: id});
+})
